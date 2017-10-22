134b020cd1fd5a3b8cdc64975960848462ee3072
xiedacon <xiedacon@qq.com>
2017-10-22 20:36:27 +0800

    chore: 去除多余 config

--- config.js
+++ config.js
@@ -7,13 +7,10 @@ exports.dingding = {
   ]
 }
 
-// TODO: 简化参数
 exports.coding = {
   cookie: 'Coding 的 Cookie',
   domain: 'Coding 企业版域名',
-  user: '企业版用户名',
   project: '需要添加机器人的项目名',
-  projectId: '需要添加机器人的项目ID',
   developers: ['开发者']
 }
 
