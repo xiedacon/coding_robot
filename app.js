357e7547312280b8c7c157efc81202bc6752e218
xiedacon <xiedacon@qq.com>
2017-12-10 17:54:18 +0800

    feat: 使用 node-schedule 做周期控制

--- app.js
+++ app.js
@@ -2,24 +2,16 @@
 
 const fs = require('fs')
 const { resolve } = require('path')
+const schedule = require('node-schedule')
 
 const dingding = require('./lib/dingding')
 const coding = require('./lib/coding')
 const { sendMsgs } = require('./lib/msg')
 const { core } = require('./config')
 
-const interval = core.interval
-
-let before = Date.now();
-
-(function loop () {
-  setTimeout(async () => {
-    let _before = before
-    before = Date.now()
-    await sendMsgs(_before)
-    loop()
-  }, interval * 1000)
-})()
+schedule.scheduleJob(core.schedule, async () => {
+  await sendMsgs(Date.now())
+})
 
 process.on('unhandledRejection', async (reason, promise) => {
   fs.writeFileSync(resolve(__dirname, './error'), reason.stack)
