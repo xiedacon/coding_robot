4c5abb108c0dba59fdd276dd278ae69b2183473d
xiedacon <xiedacon@qq.com>
2018-03-11 19:15:04 +0800

    test: append test cases for render.render

---
+++ test/render/markdown/render.test.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const test = require('ava').default
+const rewire = require('rewire')
+
+const config = require('../../../config')
+const Markdown = rewire('../../../render/markdown')
+
+const asset = {
+  title: 'test',
+  lines: ['line1', 'line2', 'line3'],
+  ats: ['test1', 'test2']
+}
+
+test('it should work', (t) => {
+  let mk = new Markdown(config)
+
+  t.deepEqual(mk.render(asset.title, asset.lines), {
+    'msgtype': 'markdown',
+    'markdown': {
+      title: asset.title,
+      text: asset.lines.join(Markdown.__get__('enter'))
+    },
+    'at': {
+      'atMobiles': [],
+      'isAtAll': false
+    }
+  })
+})
+
+test('it should work when @someone', (t) => {
+  let mk = new Markdown(Object.assign({}, config, {
+    dingding: {
+      users: [
+        { realname: 'test1', name: '110' },
+        { realname: 'test2', name: '120' }
+      ]
+    }
+  }))
+
+  t.deepEqual(mk.render(asset.title, [
+    'line1 [@test1]()',
+    'line2 @test2',
+    'line2 @test1'
+  ]), {
+    'msgtype': 'markdown',
+    'markdown': {
+      title: asset.title,
+      text: [
+        'line1 @110',
+        'line2 @120',
+        'line2 @110'
+      ].join(Markdown.__get__('enter'))
+    },
+    'at': {
+      'atMobiles': ['110', '120'],
+      'isAtAll': false
+    }
+  })
+})
+
+test('it should work when two @someone inline', (t) => {
+  let mk = new Markdown(Object.assign({}, config, {
+    dingding: {
+      users: [
+        { realname: 'test1', name: '110' },
+        { realname: 'test2', name: '120' }
+      ]
+    }
+  }))
+
+  t.deepEqual(mk.render(asset.title, [
+    'line1 [@test1]() @test1',
+    'line2 @test2',
+    'line2 @test1'
+  ]), {
+    'msgtype': 'markdown',
+    'markdown': {
+      title: asset.title,
+      text: [
+        'line1 @110 @110',
+        'line2 @120',
+        'line2 @110'
+      ].join(Markdown.__get__('enter'))
+    },
+    'at': {
+      'atMobiles': ['110', '120'],
+      'isAtAll': false
+    }
+  })
+})
