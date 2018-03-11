86ba8bed60cbcb2a479a9522500c4c24b7c68109
xiedacon <xiedacon@qq.com>
2018-03-11 14:59:37 +0800

    test: append test cases for store

---
+++ test/store/index.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const test = require('ava').default
+const rewire = require('rewire')
+
+const Store = rewire('../../lib/store')
+
+test('it should work', async (t) => {
+  let store = new Store()
+
+  await store.set('test', 'test')
+
+  t.is(store.get('test'), 'test')
+})
+
+test('it should clear store', async (t) => {
+  let store = new Store({
+    clear: 10
+  })
+
+  await store.set('test', 'test')
+
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, 10)
+  })
+
+  t.is(await store.get('test'), undefined)
+})
