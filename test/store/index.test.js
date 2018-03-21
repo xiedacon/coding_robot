1311069d7c48b24fb296b0f07e24678fb677e5dd
xiedacon <xiedacon@qq.com>
2018-03-21 09:27:23 +0800

    test: adjust test for store

--- test/store/index.test.js
+++ test/store/index.test.js
@@ -12,19 +12,3 @@ test('it should work', async (t) => {
 
   t.is(await store.get('test'), 'test')
 })
-
-test('it should clear store', async (t) => {
-  let store = new Store({
-    clear: 10
-  })
-
-  await store.set('test', 'test')
-
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve()
-    }, 10)
-  })
-
-  t.is(await store.get('test'), undefined)
-})
